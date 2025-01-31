b6d974804db1e98859ec1424d1d7e3300d952004
joshitha-30 <joshitha301@gmail.com>
2025-01-31 08:40:22 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+                 
+public class Arithmetic{
+    public static void main(String args[]) {
+    int first, second, add, subtract, multiply;
+    float divide;
+    Scanner scanner = new Scanner(System.in);
+    first = scanner.nextInt();
+    second = scanner.nextInt();
+
+    add = first + second;
+    subtract = first - second;
+    multiply = first * second;
+    divide = (float) first / second;
+
+    System.out.println("Sum = " + add);
+    System.out.println("Difference = " + subtract);
+    System.out.println("Multiplication = " + multiply);
+    System.out.println("Division = " + divide);
+  }
+}
+    
+
